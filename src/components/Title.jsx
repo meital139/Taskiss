216cbc51bc7a191ea6e8a4660038dd38a41722a5
meital babay <meital139@gmail.com>
2022-12-12 23:31:47 +0200

    upadate new logo

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -11,8 +11,8 @@ export default function Title() {
   return (
     <div id='header'>
         <img className='logo' src={taskissLogo} alt="Logo" />
-        <div className='loving'>Loving your Todo's list!</div>
-        <div className='meital'>By Meital Babay</div>
+        {/* <div className='loving'>Loving your Todo's list!</div>
+        <div className='meital'>By Meital Babay</div> */}
     </div>
   )
 }
